01bf987e47ac26fcc05142e969add4a4044dccb3
Edgar Barocio <edgar.barocio@gmail.com>
2020-06-04 11:02:26 -0400

    add model base file

--- GifSearcher/GifSearcher/Models/AnimatedGif.swift
+++ GifSearcher/GifSearcher/Models/AnimatedGif.swift
@@ -2,8 +2,7 @@
 //  AnimatedGif.swift
 //  GifSearcher
 //
-//  Created by Edgar Barocio on 6/4/20.
-//  Copyright © 2020 Edgar Barocio. All rights reserved.
+//  Created on 6/4/20.
 //
 
 import Foundation
