9d7ba2ee7204ba8492d15bd32074b917b7fa695c
Edgar Barocio <edgar.barocio@gmail.com>
2020-06-04 09:19:04 -0400

    Initial commit, create project

---
+++ GifSearcher/GifSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GifSearcher
+//
+//  Created by Edgar Barocio on 6/4/20.
+//  Copyright © 2020 Edgar Barocio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
