8031f3c4f251af28509c7ebd73e62a2f0c3d135e
Edgar Barocio <edgar.barocio@gmail.com>
2020-06-05 01:09:20 -0400

    remove carthage

---
+++ GifSearcher/GifSearcher/Views/SearchResultCell.swift
@@ -0,0 +1,42 @@
+//
+//  SearchResultCell.swift
+//  GifSearcher
+//
+//  Created  on 6/5/20.
+//
+
+import UIKit
+import FLAnimatedImage
+
+class SearchResultCell: UICollectionViewCell {
+    
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    @IBOutlet var imageView: FLAnimatedImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        update(with: nil)
+    }
+    
+    func update(with url: String?) {
+        if let urlOfGif = url {
+            
+            let url = URL(string: urlOfGif)!
+            let imageData = try? Data(contentsOf: url)
+            let animatedGif = FLAnmatedIamge(animatedGifData: imageData)
+            imageView.animatedImage = animatedGif
+            spinner.stopAnimating()
+        } else {
+            spinner.startAnimating()
+            
+        }
+    }
+}
